422eba346d94f8f9c4482f022ad3db3339018af2
bp <bp@bpsite.xyz>
2026-03-03 21:43:09 -0800

    speed up compilation time, dont use sanitizers

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 VERSION=1.1
 
-CFLAGS= -Iinclude -O2 -Wall -march=native -g -pipe -D_FORTIFY_SOURCE=2 -fstack-protector-strong -fstack-clash-protection -fomit-frame-pointer -fsanitize=address
-#CFLAGS= -O2 -Wall -march=native -g -pipe 
+#CFLAGS= -Iinclude -O2 -Wall -march=native -g -pipe -D_FORTIFY_SOURCE=2 -fstack-protector-strong -fstack-clash-protection -fomit-frame-pointer -fsanitize=address
+CFLAGS= -Iinclude -O2 -Wall -march=native -g -pipe 
 LDFLAGS=
 
 CC=g++
