30efded731bb1a4c555664bb011bdd42cd15c702
bp <bp@bpsite.xyz>
2026-04-13 14:04:03 -0700

    redefine constructor for size_t

--- src/ringbuffer.cpp
+++ src/ringbuffer.cpp
@@ -1,13 +1,18 @@
 #include "ringbuffer.hpp"
+#include <cstdlib>
 
 template <typename T>
-RingBuffer::RingBuffer(size_t size) {
-  buffer.resize(size, 0);
+RingBuffer<T>::RingBuffer(size_t capacity) {
+  RingBuffer::capacity = capacity;
+  RingBuffer::head = 0;
+  RingBuffer::tail = 0;
+  buffer.resize(capacity + 1, 0);
 
-};
-void RingBuffer::push(T value) {
-  
-  return;
+}
+template <typename T>
+void RingBuffer<T>::push_front(T value) {
+  // buffer[head+1];
+  head = (head + 1) % capacity;
 }
 // T& operator[](size_t i);
 // const T& operator[](size_t i) const;
